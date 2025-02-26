f21a06f9239ab24435a47d5ab59f1aad0e23eb3b
cysnies <cysnies@outlook.com>
2025-02-27 00:36:54 +0800

    特性: 新增图标

--- src/config.js
+++ src/config.js
@@ -1,28 +1,17 @@
 export default [
   {
-    name: '百度',
+    name: '冰糖奇遇记',
     imageUrl: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
-    url: 'https://www.baidu.com',
+    url: 'https://wg.lawxq.cn',
   },
   {
-    name: '谷歌',
+    name: '张三的暑假',
     imageUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-    url: 'https://www.google.com',
-  },
-  // 可以添加更多条目来测试滚动条效果
-  {
-    name: '示例条目 3',
-    imageUrl: 'https://via.placeholder.com/150',
-    url: 'https://example.com',
+    url: 'https://wg2.lawxq.cn',
   },
   {
-    name: '示例条目 4',
-    imageUrl: 'https://via.placeholder.com/150',
-    url: 'https://example.com',
-  },
-  {
-    name: '示例条目 5',
-    imageUrl: 'https://via.placeholder.com/150',
-    url: 'https://example.com',
-  },
+    name: '天下没有免费的午餐',
+    imageUrl: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
+    url: 'https://wg3.lawxq.cn',
+  }
 ];
